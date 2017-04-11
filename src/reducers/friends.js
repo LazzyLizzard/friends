01cd8d2e7f7397b7dffea7af5d8c9701c872fb92
Sergei_Frolov1 <lazy.lizard@yandex.ru>
2017-04-11 12:56:34 +0300

    friends list

--- src/reducers/friends.js
+++ src/reducers/friends.js
@@ -1,5 +1,22 @@
+const initalState = {
 
-const friends = (state = {}, action) => {
+    friends: [1, 2],
+    friendsById: {
+        1: {
+            id: 1,
+            firstName: "user",
+            lastName: "one"
+        },
+        2: {
+            id: 2,
+            firstName: "john",
+            lastName: "doe"
+        }
+    }
+
+};
+
+const friends = (state = initalState, action) => {
 
     console.log(state);
 
